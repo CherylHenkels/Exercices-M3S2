2124ed230be4fca44be151280c43827029a35695
CherylHenkels <cherylhenkelss@gmail.com>
2024-09-01 14:11:38 -0300

    Setup inicial

---
+++ src/main/java/br/fullstack/education/sugestoes/M3S2Application.java
@@ -0,0 +1,13 @@
+package br.fullstack.education.sugestoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3S2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M3S2Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/fullstack/education/sugestoes/M3S2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fullstack.education.sugestoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3S2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
